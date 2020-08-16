c10471076f2846be08d37fd117ca06d504bfae4d
Kaisha Jones <KaishaJones@Samys-MBP.home>
2020-08-16 19:09:57 -0400

    updated package.json

---
+++ public/precache-manifest.7362a14955814bd63978a34a516e440f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ce34db14d69eaf733640eeb2fc86ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7eeff093221781aedf",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c4bd3b545d6a5f2eb46b",
+    "url": "/static/css/main.ecd5c8fc.chunk.css"
+  },
+  {
+    "revision": "cc7eeff093221781aedf",
+    "url": "/static/js/2.b0ed7a59.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b0ed7a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bd3b545d6a5f2eb46b",
+    "url": "/static/js/main.9a95a0a3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
